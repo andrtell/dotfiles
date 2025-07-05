263345f6a0b45e204ea4ed342e64d7eafb09ce34
andrtell <116459057+andrtell@users.noreply.github.com.>
2025-07-05 16:29:41 +0200

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,7 +37,7 @@ do
 	local o = vim.opt
 
 	o.breakindent = true
-	o.background  = "dark"
+	o.background  = "light"
 	o.cursorline  = false
 	o.gdefault    = true
 	o.laststatus  = 3
@@ -49,6 +49,7 @@ do
 	o.showmode    = false
 	o.signcolumn  = "yes:1"
 	o.smartcase   = true
+	o.ignorecase  = true
 	o.swapfile    = false
 	o.timeoutlen  = 300
 	o.updatetime  = 250
@@ -59,6 +60,7 @@ do
 	end)
 end
 
+
 ------------------------------------------------------------
 -- KEYS
 ------------------------------------------------------------
@@ -66,7 +68,7 @@ end
 do
 	local g = vim.g
 
-	g.mapleader = " "
+	g.mapleader = "\\"
 	g.maplocalleader = ","
 
 	i("jk", "<esc>")
@@ -86,7 +88,7 @@ do
 	n("<S-l>",     ":bnext<cr>")
 	n("s",         "<Plug>(leap)")
 	n("S",         "<Plug>(leap-from-window)")
-
+	-- n("\\",        ":NvimTreeToggle<CR>")
 	n("]d", function () vim.diagnostic.goto_next {float=false} end)
 	n("[d", function () vim.diagnostic.goto_prev {float=false} end)
 end
@@ -119,34 +121,27 @@ require("lazy").setup({
 	ui = {
 		border = "double",
 	},
-	install = { colorscheme = { "default" } },
-	checker = { enabled = true },
+	checker = { enabled = false },
 	spec = {
 		'mhinz/vim-sayonara', 
 		{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 		{"neovim/nvim-lspconfig"},
-		{
-			'moonbit-community/moonbit.nvim',
-			ft = { 'moonbit' },
-			opts = {
-				-- optionally disable the treesitter integration
-				treesitter =  {
-					enabled = true,
-					-- Set false to disable automatic installation and updating of parsers.
-					auto_install = true
-				},
-				-- configure the language server integration
-				-- set `lsp = false` to disable the language server integration
-				lsp = {
-					-- provide an `on_attach` function to run when the language server starts
-					on_attach = function(client, bufnr) end,
-					-- provide client capabilities to pass to the language server
-					capabilities = vim.lsp.protocol.make_client_capabilities(),
-				}
-			},
-		}
+		{"ggandor/leap.nvim"},
+        {"Olical/conjure"},
 	}
 })
+
+------------------------------------------------------------
+-- CONJURE
+------------------------------------------------------------
+
+do
+    local g = vim.g
+
+    g['conjure#filetype#scheme'] = 'conjure.client.guile.socket'
+    g['conjure#client#guile#socket#pipename'] = "/home/tell/tmp/guile/.guile-repl-socket"
+
+end
 
 ------------------------------------------------------------
 -- TREESITTER
@@ -155,9 +150,9 @@ require("lazy").setup({
 local config = require("nvim-treesitter.configs")
 
 config.setup({
-  ensure_installed = {"lua", "javascript", "moonbit"},
+  ensure_installed = {"lua", "javascript", "ada"},
   highlight = { enable = true },
-  indent = { enable = true }
+  indent = { enable = false }
 })
 
 ------------------------------------------------------------
@@ -176,17 +171,11 @@ do
 		group="ALL",
 		callback=function ()
 			local opt = { silent=true, buffer=true, remap=true }
-
 			n("<esc>", ":Sayonara!<cr>",  opt)
 			n("h",     "-",               opt)
 			n("l",     "<cr>",            opt)
 			n(".",     "gh",              opt)
 			n("H",     "h",               opt)
-
-			hi("CursorLine", {bg="#efefef"})
-			hi("NetrwDir", {bg="none"})
-			hi("NetrwExe", {bg="none"})
-			hi("NetrwClassify", {fg="#008080"})
 		end,
 	})
 end
@@ -200,93 +189,62 @@ do
 
 	function grey(v) return hsv(0, 0, v) end
 
-	local text = {
-		black = hsv(0, 0, 0.13),
-		red = hsv(0, 0.745, 0.595),
-		green = hsv(0.35, 0.92, 0.385),
-		blue = hsv(0.66, 0.785, 0.60),
-		purple = hsv(0.83, 0.7, 0.46),
-	}
-
-	local fill = {
-		white = hsv(0, 0, 0.996),
-		blue = hsv(0.55, 0.1, 0.97),
-		yellow = hsv(0.14, 0.19, 0.985), -- '#faf2d8'
-	}
+	function invisible(col) return {fg = col, bg = col } end
 
 	function fg (col) return {fg = col} end
 	function bg (col) return {bg = col} end
 
-	function invisible(col) return {fg = col, bg = col } end
+	black = hsv(0, 0, 0.110)
+	white = hsv(0, 0, 0.96)
+
+	local fill = {
+		white = hsv(0, 0, 0.97),
+		yellow = hsv(0.16, 0.20, 0.93),
+	}
+
+	local text = {
+		blue = hsv(0.59, 0.82, 0.47),
+		green = hsv(0.36, 0.91, 0.425),
+		orange = hsv(0.08, 0.73, 0.57),
+		purple = hsv(0.84, 0.54, 0.61),
+		red = hsv(0, 0.665, 0.59),
+	}
 
 	local theme = {
 		vim = {
-			[fg(grey(0.90))] = {
-				'winseparator',
-				'floatborder'
-			},
-			[bg(grey(0.875))] = {
+			[bg(grey(0.825))] = {
 				'PMenuSel'
 			},
-			[bg(grey(0.90))] = {
-				'statusline',
-			},
-			[bg(grey(0.948))] = {
+			[fg(grey(0.825))] = {
+				'winseparator',
+				'floatborder',
+            },
+			[bg(grey(0.912))] = {
 				'visual', 
 				'visualnos',
-				'PMenu'
 			},
-			[invisible(fill.white)] = {
+			[bg(grey(0.89))] = {
+				'PMenu',
+				'statusline',
+			},
+			[{bg=none}] = {
+				'PmenuMatch', 
+				'PMenuMatchSel'
+			},
+			[invisible(white)] = {
 				'endofbuffer'
 			},
-			[bg(fill.blue)] = {
-				'matchparen'
+			[bg(fill.yellow)] = {
+				'matchparen',
+			},
+			[{ bg = grey(0.89), fg = black }] = {
+                'LeapLabel',
 			},
 			[bg(fill.yellow)] = {
 				'cursearch', 
 				'incsearch', 
 				'search'
 			},
-			[{bg=none}] = {
-				'PmenuMatch', 
-				'PMenuMatchSel'
-			}
-		},
-		code = {
-			[fg(grey(0.62))] = { -- "#9c9ea3"
-				'comment',
-				'@comment'
-			}
-		},
-		lua = {
-			[fg(text.red)] = {
-				'@number.lua'
-			},
-			[fg(text.green)] = {
-				'@string.lua',
-				'@string.escape.lua',
-			},
-			[fg(text.blue)] = {
-				'@keyword.conditional.lua',
-				'@keyword.function.lua',
-				'@keyword.lua',
-				'@keyword.operator.lua',
-				'@keyword.repeat.lua',
-				'@keyword.return.lua',
-			}
-		},
-		moonbit = {
-			[fg(text.green)] = {
-				'@string.moonbit',
-			},
-			[fg(text.blue)] = {
-				'@keyword.modifier.moonbit',
-				'@keyword.function.moonbit',
-			},
-			[fg(text.red)] = {
-				'@type.moonbit',
-				'@type.builtin.moonbit',
-			}
 		},
 		lazy = {
 			[bg(grey(0.87))] = {
@@ -297,13 +255,117 @@ do
 				'LazySpecial',
 				'LazyH1'
 			}
-		}
+		},
+		netrw = {
+			[fg(text.blue)] = {
+				'NetrwClassify'
+			}
+		},
+        code = {
+			[fg(grey(0.525))] = {
+				'comment',
+				'@comment',
+			},
+        },
+        bash = {
+			[fg(text.blue)] = {
+				'@string.bash',
+			},
+			[fg(text.red)] = {
+				'@variable.bash',
+				'@variable.builtin.bash',
+				'@constant.bash',
+			},
+        },
+        lua = {
+			[fg(text.blue)] = {
+				'@string.lua',
+				'@string.escape.lua',
+			},
+			[fg(text.green)] = {
+				'@boolean.lua',
+			},
+			[fg(text.red)] = {
+				'@number.lua',
+			},
+			[{fg = text.green, bold = true}] = {
+				'@keyword.lua',
+				'@keyword.conditional.bash',
+				'@keyword.conditional.lua',
+				'@keyword.function.lua',
+				'@keyword.operator.lua',
+				'@keyword.repeat.bash',
+				'@keyword.repeat.lua',
+				'@keyword.return.lua',
+			},
+        },
+        scheme = {
+			[{fg = text.green, bold = true}] = {
+                '@function.builtin.scheme',
+                '@keyword.scheme',
+                '@keyword.conditional.scheme',
+			},
+			[{fg = text.green, bold = false}] = {
+                '@operator.scheme',
+			},
+			[fg(text.blue)] = {
+				'@string.scheme',
+			},
+			[fg(text.green)] = {
+				'@boolean.scheme',
+			},
+			[fg(text.red)] = {
+				'@number.scheme',
+			},
+			-- [fg(grey(0.3))] = {
+			-- 	'@punctuation.bracket.scheme',
+			-- },
+        },
+        c = {
+			[{fg = text.green, bold = true}] = {
+                '@keyword.import.c',
+                '@keyword.modifier.c',
+                '@keyword.return.c',
+                '@keyword.conditional.c',
+			},
+			[{fg = text.purple, bold = true}] = {
+                '@type.builtin.c',
+			},
+			[fg(text.blue)] = {
+				'@string.c',
+			},
+			[fg(text.red)] = {
+				'@number.c',
+			},
+        },
+        gleam = {
+			[{fg = text.green, bold = true}] = {
+				'@keyword.gleam',
+				'@keyword.conditional.gleam',
+				'@keyword.modifier.gleam',
+				'@keyword.import.gleam',
+				'@keyword.function.gleam',
+				'@keyword.exception.gleam',
+			},
+			[{fg = text.blue, bold = true }] = {
+				'@string.gleam',
+			},
+			[{fg = text.red }] = {
+				'@number.gleam',
+			},
+			[{fg = text.purple, bold = true }] = {
+				'@type.gleam',
+			},
+			[{fg = text.orange, bold = true}] = {
+				'@function.call.gleam',
+			},
+        },
 	}
 
 	function clear_hl ()
 		for hg, _ in pairs(vim.api.nvim_get_hl(0, {})) do
 			if type(hg) == "string" then
-				hi(hg, {fg = text.black, bg = fill.white})
+				hi(hg, {fg = black, bg = white})
 			end
 		end
 	end
@@ -322,39 +384,30 @@ do
 		group="ALL",
 		pattern='default',
 		callback=function () 
-			clear_hl()
+            clear_hl()
 			set_hl()
 		end
 	})
 
-	vim.cmd("colorscheme default")
+    vim.cmd("colorscheme default")
 end
 
--- local set_highlights = function() 
-	-- 	-- tiny
-	-- 	hi("TinyInlineDiagnosticVirtualTextError", {bg="#f0f0f1"})
-	-- 	hi("TinyInlineDiagnosticVirtualTextWarn", {bg="#f0f0f1"})
-	-- 	hi("TinyInlineDiagnosticVirtualTextHint", {bg="#f0f0f1"})
-	-- 	hi("TinyInlineDiagnosticVirtualTextArrow", {fg="#efeff1"})
-	--
-	-- 	-- leap
-	-- 	hi("LeapLabelPrimary",         {bg="#fadffa"})
-	--
-	-- 	-- diagnostic
-	-- 	hi("DiagnosticUnderlineError", {underline=true, sp="#F00000"})
-	-- 	hi("DiagnosticUnderlineWarn",  {underline=true, sp="#FF0000"})
-	-- 	hi("DiagnosticUnderlineInfo",  {underline=true, sp="#FF0000"})
-	-- 	hi("DiagnosticUnderlineHint",  {underline=true, sp="#FF0000"}) 
-	-- 	hi("DiagnosticUnnecessary",    {underline=true, sp="#FF0000"}) 
-	-- 	hi("DiagnosticDeprecated",     {underline=true, sp="#FF0000"})
-	-- 	hi("DiagnosticFloatingError",  {fg="#030303"})
-	-- 	hi("DiagnosticError",          {bg="#fbe5e5"})
-	-- 	hi("DiagnosticWarn",           {bg="#fbe5e5"})
-	--
-	-- 	-- hi("SnippetTabStop", {bg="#faf2d8"}) 
-	-- end
-
-	-- set_highlights()
-
-	-- end
-
+-- -- 	-- tiny
+-- -- 	hi("TinyInlineDiagnosticVirtualTextError", {bg="#f0f0f1"})
+-- -- 	hi("TinyInlineDiagnosticVirtualTextWarn", {bg="#f0f0f1"})
+-- -- 	hi("TinyInlineDiagnosticVirtualTextHint", {bg="#f0f0f1"})
+-- -- 	hi("TinyInlineDiagnosticVirtualTextArrow", {fg="#efeff1"})
+-- -- 	-- diagnostic
+-- -- 	hi("DiagnosticUnderlineError", {underline=true, sp="#F00000"})
+-- -- 	hi("DiagnosticUnderlineWarn",  {underline=true, sp="#FF0000"})
+-- -- 	hi("DiagnosticUnderlineInfo",  {underline=true, sp="#FF0000"})
+-- -- 	hi("DiagnosticUnderlineHint",  {underline=true, sp="#FF0000"}) 
+-- -- 	hi("DiagnosticUnnecessary",    {underline=true, sp="#FF0000"}) 
+-- -- 	hi("DiagnosticDeprecated",     {underline=true, sp="#FF0000"})
+-- -- 	hi("DiagnosticFloatingError",  {fg="#030303"})
+-- -- 	hi("DiagnosticError",          {bg="#fbe5e5"})
+-- -- 	hi("DiagnosticWarn",           {bg="#fbe5e5"})
+-- --
+-- -- 	-- hi("SnippetTabStop", {bg="#faf2d8"}) 
+-- -- end
+--
